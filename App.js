962d39f75cb2c5907d9ce762661f9cfae1dac2c3
maliyilmaz0 <mehmetaliyilmaz5371@gmail.com>
2023-06-12 18:29:21 +0300

    Nothing changes

--- App.js
+++ App.js
@@ -3,17 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your!</Text>
+    <View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
